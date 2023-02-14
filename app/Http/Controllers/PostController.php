33398b0995c26fd0a022310492372130ea928cb0
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-02-14 19:44:02 +0100

    add image a post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+// use pagination;
+use validation;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -15,9 +17,12 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::with('category')->get();
+        // $posts = Post::with('category')->get();
 
-        return view('posts.index', compact('posts'));
+        // return view('posts.index', compact('posts'));
+        $posts = Post::latest()->paginate(5);
+
+        return view('posts.index', compact('posts'))->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -40,13 +45,32 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        Post::create([
-            'title' => $request->input('title'),
-            'post_text' => $request->input('post_text'),
-            'category_id' => $request->input('category_id'),
+        // Post::create([
+        //     'title' => $request->input('title'),
+        //     'post_text' => $request->input('post_text'),
+        //     'category_id' => $request->input('category_id'),
+        // ]);
+
+        // return redirect()->route('posts.index');
+
+        $request->validate([
+            'title' => 'required',
+            'post_text' => 'required',
+            'category_id' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
-        return redirect()->route('posts.index');
+        $input = $request->all();
+
+        if ($image = $request->file('image')) {
+            $destinationPath = 'images/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }
+
+        Post::create($input);
+        return redirect()->route('posts.index')->with('success', 'Post created successfully.');
     }
 
     /**
@@ -82,13 +106,34 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        $post->update([
-            'title' => $request->input('title'),
-            'post_text' => $request->input('post_text'),
-            'category_id' => $request->input('category_id'),
+        // $post->update([
+        //     'title' => $request->input('title'),
+        //     'post_text' => $request->input('post_text'),
+        //     'category_id' => $request->input('category_id'),
+        // ]);
+
+        // return redirect()->route('posts.index');
+
+        $request->validate([
+            'title' => 'required',
+            'post_text' => 'required',
+            'category_id' => 'required'
         ]);
 
-        return redirect()->route('posts.index');
+        $input = $request->all();
+
+        if ($image = $request->file('image')) {
+            $destinationPath = 'images/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        } else {
+            unset($input['image']);
+        }
+
+        $post->update($input);
+
+        return redirect()->route('posts.index')->with('success', 'Post updated successfully');
     }
 
     /**
@@ -99,8 +144,11 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        $post->delete();
+        // $post->delete();
 
-        return redirect()->route('posts.index');
+        // return redirect()->route('posts.index');
+        $post->delete();
+     
+        return redirect()->route('posts.index')->with('success','Post deleted successfully');
     }
 }
